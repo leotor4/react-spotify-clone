a296c90d7b70d50dd0fd925f4d343b1a6eecbfc0
PEDRO TORQUATO <leotorquato@edu.unifor.br>
2021-09-14 21:33:50 -0300

    correcao index

--- src/index.js
+++ src/index.js
@@ -2,15 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css'
 import Home from "./components/pages/Home"
-import Header from './components/Header';
 import reportWebVitals from './reportWebVitals';
-import Footer from './components/Footer';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Header />
     <Home />
-    <Footer />
   </React.StrictMode>,
   document.getElementById('root')
 );
